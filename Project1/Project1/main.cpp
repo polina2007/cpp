9143ff51200cdf727f0721109143c4e3f2098ea7
polina2007 <a.s.donenko@gmail.com>
2019-10-06 20:16:04 +0300

    product of digit and max sum of digit

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -15,8 +15,46 @@ int sumOfDigit(int number)
 	return e + d + s;
 }
 
+int invertedNumber(int number)
+{
+	int e, d, s;
+	
+	e = number % 10;
+	number /= 10;
+	d = number % 10;
+	number /= 10;
+	s = number % 10;
+
+	return e * 100 + d * 10 + s;
+}
+
+int prodOfDigit(int number)
+{
+	int e, d, s;
+
+	e = number % 10;
+	number /= 10; // number = number / 10
+	d = number % 10;
+	number /= 10;
+	s = number % 10;
+
+	return e * d * s;
+}
+
+int maxSumOfDigit(int number1, int number2)
+{
+	if (sumOfDigit(number1) > sumOfDigit(number2))
+	{
+		return number1;
+	}
+	else
+	{
+		return number2;
+	}
+}
+
 int main()
 {
-	cout << sumOfDigit(125);
+	cout << prodOfDigit(456);
 	return 0;
 }
