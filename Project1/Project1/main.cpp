547ad315e3345abfac1cd720065f0926e8dc5feb
polina2007 <a.s.donenko@gmail.com>
2019-10-06 21:04:08 +0300

    lessond end

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -80,6 +80,18 @@ int task1(int n1, int n2)
 	return n1 % n2;
 }
 
+// получает целое число, возвращает его пятнадцатую степень
+int power15(int n)
+{
+	int m = n;
+	n *= n;
+	n *= n;
+	n *= n;
+	n *= n;
+	n /= m;
+	return n;
+}
+
 int main()
 {
 	cout << maxSumOfDigit(456, 650);
